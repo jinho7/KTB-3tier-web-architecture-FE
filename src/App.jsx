b97ece180f0eb8dcc11e50fc8f7ad735f483b62e
jinho7 <69239293+jinho7@users.noreply.github.com>
2025-03-31 14:42:49 +0900

    initial: vite react setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>✅ S3 정적 웹 호스팅 성공</h1>
+      <p>이 페이지는 Vite + React 기반으로 만들어졌고, AWS S3에서 서빙 중입니다.</p>
+    </div>
+  );
+}
+
+export default App;
